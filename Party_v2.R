4c64073360a348e2db75a2bc59527729c4bc1fb5
Nathan Kim <nakim@ucsb.edu>
2025-09-17 19:31:14 +0000

    Updated contact information to include all DataContact* columns

--- Party_v2.R
+++ Party_v2.R
@@ -8,19 +8,9 @@ csv_path <- here("data", "RAProjects.csv")
 projects  <- read_csv(csv_path, show_col_types = FALSE)
 
 # creating loader table ---------------------------------------------------
-# getting number of rows
-nrow <-  nrow(projects)
-# create blank data frame (probably better way to do this)
-party_LT <- data.frame(py_code = rep(NA, nrow),
-                      surname = rep(NA, nrow),
-                      organization_name = rep(NA, nrow),
-                      given_name = rep(NA, nrow),
-                      middle_name = rep(NA, nrow),
-                      party_type = rep(NA, nrow),
-                      org_position = rep(NA, nrow),
-                      email = rep(NA, nrow),
-                      orcid = rep(NA, nrow),
-                      ror = rep(NA, nrow))
+# create a header csv with only variable names
+csv_path <- here("loader_tables", "Party_Header.csv")
+template <- read_csv(csv_path, show_col_types = FALSE)
 
 # Checking values ---------------------------------------------------------
 
@@ -42,21 +32,33 @@ unique(invalid_emails$DataContactEmail)
 # All email and name values are valid
 
 # Tidying Data -------------------------------------------------------
+# Changing to RAProjects to long format to have one row per contact
+
+projects_long <- projects %>%
+  pivot_longer(
+    cols = matches("^DataContact(Name|Email|Organization)\\d*$"),
+    names_to   = c(".value", "slot"),
+    names_pattern = "^DataContact(Name|Email|Organization)(\\d*)$"
+  ) %>%
+  filter(!is.na(Name) & str_squish(Name) != "") %>%
+  transmute(
+    ContactName  = str_squish(Name),
+    ContactEmail = str_squish(Email),
+    ContactOrg = str_squish(Organization)
+  )
+
 # Separating DataContactName into `FirstName` and `LastName`.
 
-project_name_split <- projects %>%
-  separate(DataContactName, into = c("FirstName", "LastName"), sep = " ", extra = "merge")
+project_name_split <- projects_long %>%
+  separate(ContactName, into = c("FirstName", "LastName"), sep = " ", extra = "merge")
 
 # Assigning columns to loader table ---------------------------------------
-party_LT$py_code <- 'NA'
-party_LT$surname <- project_name_split$LastName
-party_LT$organization_name <- project_name_split$DataContactOrganization
-party_LT$given_name <- project_name_split$FirstName
-party_LT$middle_name <- 'NA'
-party_LT$party_type <- 'NA'
-party_LT$org_position <- 'NA'
-party_LT$email <- project_name_split$DataContactEmail
-party_LT$orcid <- 'NA'
-party_LT$ror <- 'NA'
+party_LT <- bind_rows(
+  template,
+  tibble(surname = project_name_split$LastName,
+         organization_name = project_name_split$ContactOrg,
+         given_name = project_name_split$FirstName,
+         email = project_name_split$ContactEmail)
+)
 
 # py_code, middle_name, party_type, org_position, orcid, and ror not present in CDFW data.
