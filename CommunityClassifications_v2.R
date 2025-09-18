dff062bc5d1a28f1e109cb1937b950465904f030
Nathan Kim <nakim@ucsb.edu>
2025-09-18 19:41:58 +0000

    Manually assigned ClassificationDescription values

--- CommunityClassifications_v2.R
+++ CommunityClassifications_v2.R
@@ -13,32 +13,33 @@ csv_path <- here("loader_tables", "CommunityClassification_Header.csv")
 template <- read_csv(csv_path, show_col_types = FALSE)
 
 # Tidying CDFW data -------------------------------------------------------
-# Separating ClassificationDescription into boolean fields (multivariate analysis, table analsyis, and inspection)
+# Manually assigning ClassificationDescription values to inspection, multivariateAnalysis, or tableAnalysis based on the described methods.
+# None mapped to tableAnalysis
 
 projects <- projects %>%
   mutate(
-    method_inspection = str_detect(str_to_lower(ClassificationDescription), "inspection"),
-    method_multivar = str_detect(str_to_lower(ClassificationDescription), "multivariate"),
-    method_table = str_detect(str_to_lower(ClassificationDescription), "table")
+    inspectionText = if_else(
+      coalesce(str_detect(ClassificationDescription, "surveys were keyed using"), FALSE),
+      ClassificationDescription, NA_character_
+    ),
+    multivariateAnalysisText = if_else(
+      coalesce(str_detect(
+        ClassificationDescription,
+        'See "Vegetation Map and Classification of Fish|These data were analyzed using multivariate cluster analysis, performed|See: Classification of the Vegetation Alliances|These data were analyzed using a number of statistical methods, chiefly an|CNPS analyzed the species cover data using PC-Ord and R software'
+      ), FALSE),
+      ClassificationDescription, NA_character_
+    )
   )
 
 # Assigning columns to loader table ---------------------------------------
-# Changing column types to support to data
-template <- template %>%
-  mutate(
-    inspection = as.logical(inspection),
-    multivariateAnalysis = as.logical(multivariateAnalysis),
-    tableAnalysis = as.logical(tableAnalysis)
-  )
 
 CommunityClassifications_LT <- bind_rows(
   template,
   tibble(expertSystem = projects$ClassificationTool,
-         inspection = projects$method_inspection,
-         multivariateAnalysis = projects$method_multivar,
-         tableAnalysis = projects$method_table
+         inspection = projects$inspectionText,
+         multivariateAnalysis = projects$multivariateAnalysisText
   )
 )
-
+CommunityClassifications_LT
 # All variables besides expertSystem, inspection, multivariateAnalysis, and tableAnalysis were not matched and are left as 'NA'
 
