67567a01d615ca5a1d5779658d1377d7c03061dc
Nathan Kim <nakim@ucsb.edu>
2025-10-20 23:24:09 +0000

    Updated column assignment

--- SoilData_v2.R
+++ SoilData_v2.R
@@ -1,7 +1,6 @@
 library(tidyverse)
 library(here)
 library(stringr)
-source("Build_Loader_Table.R")
 
 # load in CDFW data -------------------------------------------------------
 # RAPlots
@@ -13,14 +12,9 @@ csv_path <- here("data", "LSoil.csv")
 soil_lookup <- read_csv(csv_path, show_col_types = FALSE)
 
 # creating loader table ---------------------------------------------------
-
-soil_template_fields <- build_loader_table(
-  sheet_url = "https://docs.google.com/spreadsheets/d/1ORubguw1WDkTkfiuVp2p59-eX0eA8qMQUEOfz1TWfH0/edit?gid=2109807393#gid=2109807393",
-  sheet = "SoilData",
-  source_df = plots
-)
-
-soil_LT <- soil_template_fields$template
+# create a header csv with only variable names
+csv_path <- here("loader_tables", "SoilData_Header.csv")
+template <- read_csv(csv_path, show_col_types = FALSE)
 
 # Checking values ---------------------------------------------------------
 
@@ -34,6 +28,12 @@ plots %>%
 
 # Assigning column to loader table ---------------------------------------
 
-soil_LT$soilDescription = plots$Soil_text
+SoilData_LT <- bind_rows(
+  template,
+  tibble(soilDescription = plots$Soil_text
+         )
+)
+
+SoilData_LT$soilDescription
 
 # For now there is only one variable that mapped to the Soil Data Loader table (Soil_text from RAPlots)
