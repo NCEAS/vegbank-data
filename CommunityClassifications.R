ea734311542543b8a302ead0e50849f31759a17b
Lorretta Lu <lu10@ucsb.edu>
2025-10-31 17:36:43 +0000

    Adding Confidence_ID

--- CommunityClassifications.R
+++ CommunityClassifications.R
@@ -7,6 +7,10 @@ source("Build_Loader_Table.R")
 # RAProjects
 csv_path <- here("data", "RAProjects.csv")
 projects <- read_csv(csv_path, show_col_types = FALSE)
+folder <- 'data'
+plots <- read_csv(here(folder, 'RAPlots.csv'), 
+                  col_types = cols(.default = col_guess(), 
+                                   `PlotOther5` = col_character()))
 
 # creating loader table ---------------------------------------------------
 community_template_fields <- build_loader_table(
@@ -23,7 +27,18 @@ community_LT <- community_template_fields$template
 unique(projects$ClassificationDescription)
 class(projects$ClassificationDescription) # character
 
-# Tidying CDFW data -------------------------------------------------------
+# Confidence_ID (RAPlots) - class_confidence (CommunityClassifications)
+# Convert L, M, H to Low, Medium, High
+unique(plots$Confidence_ID)
+class(plots$Confidence_ID) # character
+
+# Tidying CDFW data -----------------------------------------------------------
+
+# RAPlots and RAProjects needs to be merged
+plots_merged <- plots
+
+projects <- plots_merged %>% 
+  left_join(projects, by = "ProjectCode")
 
 ### inspection (CommunityClassifications) + multivariateAnalysis (CommunityClassifications) + tableAnalysis (CommunityClassifications ###
 # Manually assigning ClassificationDescription values to inspection, multivariateAnalysis, or tableAnalysis based on the described methods.
@@ -53,10 +68,28 @@ projects <- projects %>%
     )
   )
 
+# Confidence_ID (RAPlots) - class_confidence (CommunityClassifications)
+# Convert L, M, H to Low, Medium, High
+# Convert Not recorded to NA
+
+projects <- plots_merged %>% 
+  mutate(
+    class_confidence = case_when(
+      Confidence_ID == "Not recorded" ~ NA,
+      Confidence_ID == "H" ~ "High",
+      Confidence_ID == "M" ~ "Medium",
+      Confidence_ID == "L" ~ "Low",
+      
+      TRUE ~ NA_character_
+    )
+  )
+
 # Assigning columns to loader table ---------------------------------------
 
 community_LT$expert_system <- projects$ClassificationTool
 community_LT$inspection <- projects$inspectionText
 community_LT$multivariate_analysis <- projects$multivariateAnalysisText
+community_LT$class_confidence <- projects$class_confidence # Error, need to fix
 
 # All variables besides expertSystem, inspection, multivariateAnalysis, and tableAnalysis were not matched and are left as 'NA'
+write_csv(community_LT, here('loader_tables', 'CommunityClassificationsLT.csv'))
