ae2d07c1c325eaa82983a4f81959c1bf5c638939
Jeanette Clark <jclark@datateam.nceas.ucsb.edu>
2025-12-18 20:19:47 +0000

    wrap project code into a function, add some sanity checking for date reformatting

--- Project.R
+++ Project.R
@@ -1,50 +1,73 @@
 library(tidyverse)
-library(here)
 source("R/build_loader_table.R")
 
 # load in CDFW data -----------------------------------------------------------
+project_loader <- function(in_path, out_path){
+  
+  sub_folders <- dir(in_path, full.names = TRUE) %>%
+    grep(pattern = "VegBankProject", value = TRUE)
+  
+  # read in RAProjects
+  project_files <- dir(sub_folders, full.names = TRUE) %>% 
+    grep(pattern = "RAProjects.csv", value = TRUE)
+  
+  message(paste("Processing", length(project_files), "project tables:", paste(project_files, collapse = ", ")))
+  
+  projects_df_list <- lapply(project_files, read_csv, progress = FALSE, show_col_types = FALSE)
+  
+  projects <- do.call(bind_rows, projects_df_list)
+  
+  # creating loader table -------------------------------------------------------
+  
+  project_template_fields <- build_loader_table(
+    sheet_url = "https://docs.google.com/spreadsheets/d/1ORubguw1WDkTkfiuVp2p59-eX0eA8qMQUEOfz1TWfH0/edit?gid=2109807393#gid=2109807393",
+    sheet = "Project",
+    source_df = projects
+  )
+  
+  project_LT <- project_template_fields$template
+  
+  # tidying CDFW data -----------------------------------------------------------
+  
+  ### Converting Start Date and End Time to just Dates ###
+  projects$ProjectStartDate <- as_date(mdy_hms(projects$ProjectStartDate))
+  projects$ProjectEndDate <- as_date(mdy_hms(projects$ProjectEndDate))
+  
+  # warn if dates are before 1900
+  if (any(projects$ProjectStartDate < as.Date("1900-01-01"), na.rm = TRUE)) {
+    warning("Some project start dates are before 1900.")
+  }
+  
+  if (any(projects$ProjectEndDate < as.Date("1900-01-01"), na.rm = TRUE)) {
+    warning("Some project end dates are before 1900.")
+  }
+  
+  today <- Sys.Date()
+  
+  if (any(projects$ProjectStartDate > today, na.rm = TRUE)) {
+    warning("Some project start dates are in the future.")
+  }
+  
+  if (any(projects$ProjectEndDate > today, na.rm = TRUE)) {
+    warning("Some project end dates are in the future.")
+  }
+  
+  # assigning columns to loader table -------------------------------------------
+  
+  
+  project_LT$user_pj_code <- projects$ProjectCode
+  project_LT$project_name <- projects$ProjectName
+  project_LT$project_description <- projects$ProjectDescription
+  project_LT$start_date <- projects$ProjectStartDate
+  project_LT$stop_date <- projects$ProjectEndDate
+  
+  # save filled in loader table
+  write_csv(project_LT, out_path)
+  
+}
 
 # set folder where data is stored
-folder <- 'data'
+in_path <- '/var/data/curation/vegbank/'
+out_path <- 'data/loader-tables/projectLT.csv'
+project_loader(in_path, out_path)
 
-# read in RAProjects
-projects <- read_csv(here(folder, 'RAProjects.csv'))
-
-# creating loader table -------------------------------------------------------
-
-project_template_fields <- build_loader_table(
-  sheet_url = "https://docs.google.com/spreadsheets/d/1ORubguw1WDkTkfiuVp2p59-eX0eA8qMQUEOfz1TWfH0/edit?gid=2109807393#gid=2109807393",
-  sheet = "Project",
-  source_df = projects
-)
-
-project_LT <- project_template_fields$template
-
-# checking values -------------------------------------------------------------
-
-# ProjectStartDate and ProjectEndDate in RAProjects.csv has time
-# Start_date and stop_date in the Projects loader table is date only
-head(projects$ProjectStartDate)
-head(projects$ProjectEndDate)
-
-# tidying CDFW data -----------------------------------------------------------
-
-### Converting Start Date and End Time to just Dates ###
-# Start Date
-projects$ProjectStartDate <- as_date(mdy_hms(projects$ProjectStartDate))
-projects$ProjectStartDate
-
-# End Date
-projects$ProjectEndDate <- as_date(mdy_hms(projects$ProjectEndDate))
-projects$ProjectEndDate
-                                            
-# assigning columns to loader table -------------------------------------------
-
-project_LT$user_pj_code <- projects$ProjectCode
-project_LT$project_name <- projects$ProjectName
-project_LT$project_description <- projects$ProjectDescription
-project_LT$start_date <- projects$ProjectStartDate
-project_LT$stop_date <- projects$ProjectEndDate
-
-# save filled in loader table
-write_csv(project_LT, here('loader_tables', 'ProjectLT.csv'))
